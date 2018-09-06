8eea52ac011a291d05a26cd14108018a023cbeeb
sherazmalik06 <sherazmalik06@gmail.com>
2018-09-06 17:14:16 +0500

    Version change 1.1

---
+++ plugin-auto-update/plugin-auto-update.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ Plugin Name: Plugin Auto Update
+ Plugin URI: http://bitbytes.io
+ Description: Plugin with auto-updates from external URL/Repository
+ Author: BitBytes
+ Version: 1.1
+ */
+
+// Load the auto-update class
+add_action( 'init', 'activate' );
+function activate()
+{
+	require_once ( 'wp_autoupdate.php' );
+	$plugin_current_version = '1.0';
+	$plugin_remote_path = 'https://raw.githubusercontent.com/sherazmalik06/update-plugin/master/plugin.json';
+	$plugin_slug = plugin_basename( __FILE__ );
+	$license_user = 'user';
+	$license_key = 'abcd';
+	new WP_AutoUpdate ( $plugin_current_version, $plugin_remote_path, $plugin_slug, $license_user, $license_key );
+}
